e04e45f7a7c921115845ba567ec68113d1ffdd0e
Jewel19 <jewel.gallardo@g.batstate-u.edu.ph>
2022-10-24 04:22:57 +0800

    changes

--- src/app/ofcdash/ofcdash.page.ts
+++ src/app/ofcdash/ofcdash.page.ts
@@ -6,7 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ofcdash.page.scss'],
 })
 export class OfcdashPage implements OnInit {
+  public queuer: any= [
+    
+    
+    {qnum: 1, querid: 42344, },
+    {qnum: 2, querid: 13213, },
+    {qnum: 3, querid: 66543, },
+    {qnum: 4, querid: 91234, },
+    {qnum: 11, querid: 42344, },
+    {qnum: 21, querid: 13213, },
+    {qnum: 13, querid: 66543, },
+    {qnum: 14, querid: 91234, },
 
+]
   constructor() { }
 
   ngOnInit() {
